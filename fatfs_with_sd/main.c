7ef5e94ee64655438206a2a8b1f21a6311882cd3
Sébastien Schiesser <sebastien.schiesser@zhdk.ch>
2018-04-29 19:03:11 +0200

    Added all BLE init functions

--- fatfs_with_sd/main.c
+++ fatfs_with_sd/main.c
@@ -149,19 +149,60 @@ static uint8_t							m_rx_buf[2];
 static const uint8_t					m_length = 2;
 
 
+/* ========================================================================== */
+/*                              APP FUNCTIONS                                 */
+/* ========================================================================== */
+static void sdc_fill_queue(void)
+{
+	bsp_board_led_invert(2);
+	static FRESULT res;
+	static UINT byte_written;
+	static uint8_t p_buf[2*SDC_BLOCK_SIZE];
+	uint32_t buf_size = 2*SDC_BLOCK_SIZE;
+	uint32_t fifo_res = app_fifo_read(&m_adc2sd_fifo, p_buf, &buf_size);
+//	static app_fifo_t * p_fifo = &m_adc2sd_fifo;
+//	NRF_LOG_INFO("read: %d", p_fifo->read_pos);
+	sdc_writing = true;
+	res = f_write(&recording_fil, p_buf, (2*SDC_BLOCK_SIZE), &byte_written);
+	if(res == FR_OK) {
+		res = f_sync(&recording_fil);
+		sdc_writing = false;
+	}
+}
 
+// Start advertising
+static void advertising_start(void)
+{
+    ret_code_t           err_code;
+    ble_gap_adv_params_t adv_params;
+
+    // Start advertising
+    memset(&adv_params, 0, sizeof(adv_params));
+
+    adv_params.type        = BLE_GAP_ADV_TYPE_ADV_IND;
+    adv_params.p_peer_addr = NULL;
+    adv_params.fp          = BLE_GAP_ADV_FP_ANY;
+    adv_params.interval    = APP_ADV_INTERVAL;
+    adv_params.timeout     = APP_ADV_TIMEOUT_IN_SECONDS;
+
+    err_code = sd_ble_gap_adv_start(&adv_params, APP_BLE_CONN_CFG_TAG);
+    APP_ERROR_CHECK(err_code);
+    LED_ON(LED_ADVERTISING);
+}
+
+/* ========================================================================== */
+/*                              EVENT HANDLERS                                */
+/* ========================================================================== */
 
 void adc_spi_event_handler(nrf_drv_spi_evt_t const * p_event, void * p_context)
 {
 	static uint32_t buf_size = 2;
-//	static app_fifo_t * p_fifo = &m_adc2sd_fifo;
 	app_fifo_write(&m_adc2sd_fifo, m_rx_buf, &buf_size);
 	if(adc_spi_xfer_counter < (SDC_BLOCK_SIZE-1)) {
 		adc_spi_xfer_counter++;
 	}
 	else {
 		bsp_board_led_invert(1);
-//		NRF_LOG_INFO("write: %d", p_fifo->write_pos);
 		adc_spi_xfer_counter = 0;
 		if(!sdc_writing) {
 			sdc_rtw = true;
@@ -172,10 +213,181 @@ void adc_spi_event_handler(nrf_drv_spi_evt_t const * p_event, void * p_context)
 		}
 	}
 	adc_spi_xfer_done = true;
-//	nrf_delay_us(10);
-//	nrf_drv_spi_transfer(&adc_spi, m_tx_buf, m_length, m_rx_buf, m_length);
 }
 
+void adc_sync_timer_handler(nrf_timer_event_t event_type, void * p_context)
+{
+	bsp_board_led_invert(0);
+	if(adc_spi_xfer_done) {
+		adc_spi_xfer_done = false;
+		nrf_drv_spi_transfer(&adc_spi, m_tx_buf, m_length, m_rx_buf, m_length);
+	}
+}
+
+// BLE
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+{
+    ret_code_t err_code;
+
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_CONNECTED:
+            NRF_LOG_INFO("Connected");
+            LED_ON(LED_CONNECTED);
+            LED_OFF(LED_ADVERTISING);
+            m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            NRF_LOG_INFO("Disconnected");
+            LED_OFF(LED_CONNECTED);
+            m_conn_handle = BLE_CONN_HANDLE_INVALID;
+            advertising_start();
+            break;
+
+        case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+            // Pairing not supported
+            err_code = sd_ble_gap_sec_params_reply(m_conn_handle,
+                                                   BLE_GAP_SEC_STATUS_PAIRING_NOT_SUPP,
+                                                   NULL,
+                                                   NULL);
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_GATTS_EVT_SYS_ATTR_MISSING:
+            // No system attributes have been stored.
+            err_code = sd_ble_gatts_sys_attr_set(m_conn_handle, NULL, 0, 0);
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_GATTC_EVT_TIMEOUT:
+            // Disconnect on GATT Client timeout event.
+            NRF_LOG_DEBUG("GATT Client Timeout.");
+            err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gattc_evt.conn_handle,
+                                             BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_GATTS_EVT_TIMEOUT:
+            // Disconnect on GATT Server timeout event.
+            NRF_LOG_DEBUG("GATT Server Timeout.");
+            err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gatts_evt.conn_handle,
+                                             BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_EVT_USER_MEM_REQUEST:
+            err_code = sd_ble_user_mem_reply(p_ble_evt->evt.gattc_evt.conn_handle, NULL);
+            APP_ERROR_CHECK(err_code);
+            break;
+
+        case BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST:
+        {
+            ble_gatts_evt_rw_authorize_request_t  req;
+            ble_gatts_rw_authorize_reply_params_t auth_reply;
+
+            req = p_ble_evt->evt.gatts_evt.params.authorize_request;
+
+            if (req.type != BLE_GATTS_AUTHORIZE_TYPE_INVALID)
+            {
+                if ((req.request.write.op == BLE_GATTS_OP_PREP_WRITE_REQ)     ||
+                    (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_NOW) ||
+                    (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_CANCEL))
+                {
+                    if (req.type == BLE_GATTS_AUTHORIZE_TYPE_WRITE)
+                    {
+                        auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_WRITE;
+                    }
+                    else
+                    {
+                        auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_READ;
+                    }
+                    auth_reply.params.write.gatt_status = APP_FEATURE_NOT_SUPPORTED;
+                    err_code = sd_ble_gatts_rw_authorize_reply(p_ble_evt->evt.gatts_evt.conn_handle,
+                                                               &auth_reply);
+                    APP_ERROR_CHECK(err_code);
+                }
+            }
+        } break; // BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+
+
+// BLE connection parameter event reception
+static void on_conn_params_evt(ble_conn_params_evt_t * p_evt)
+{
+    ret_code_t err_code;
+
+    if (p_evt->evt_type == BLE_CONN_PARAMS_EVT_FAILED)
+    {
+        err_code = sd_ble_gap_disconnect(m_conn_handle, BLE_HCI_CONN_INTERVAL_UNACCEPTABLE);
+        APP_ERROR_CHECK(err_code);
+    }
+}
+
+// BLE connection parameters error
+static void conn_params_error_handler(uint32_t nrf_error)
+{
+    APP_ERROR_HANDLER(nrf_error);
+}
+
+static void led_rec_handler(uint16_t conn_handle, ble_sss_t * p_sss, uint8_t led_state)
+{
+	if(led_state) {
+		NRF_LOG_DEBUG("REC START requested");
+		if(!ui_rec_running && !ui_rec_start_req) {
+			ui_rec_start_req = true;
+		}
+		else {
+			NRF_LOG_DEBUG("REC already running...");
+		}
+	}
+	else {
+		NRF_LOG_DEBUG("REC STOP requested");
+		if(ui_rec_running || ui_rec_start_req) {
+			ui_rec_stop_req = true;
+			ui_rec_start_req = false;
+			ui_rec_running = false;
+		}
+		else {
+			NRF_LOG_DEBUG("No REC running...");
+		}
+	}
+}
+// LED MON
+static void led_mon_handler(uint16_t conn_handle, ble_sss_t * p_sss, uint8_t led_state)
+{
+	if(led_state) {
+		NRF_LOG_DEBUG("MON START requested");
+		if(!ui_mon_running && !ui_mon_start_req) {
+			ui_mon_start_req = true;
+		}
+		else {
+			NRF_LOG_DEBUG("MON already running...");
+		}
+	}
+	else {
+		NRF_LOG_DEBUG("MON STOP requested");
+		if(ui_mon_running || ui_mon_start_req) {
+			ui_mon_stop_req = true;
+			ui_mon_start_req = false;
+			ui_mon_running = false;
+		}
+		else {
+			NRF_LOG_DEBUG("No MON running...");
+		}
+	}
+}
+
+/* ========================================================================== */
+/*                                INIT/CONFIG                                 */
+/* ========================================================================== */
 static void adc_config_spi(void)
 {
 	nrf_drv_spi_config_t adc_spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
@@ -189,15 +401,6 @@ static void adc_config_spi(void)
 }
 
 
-
-void adc_sync_timer_handler(nrf_timer_event_t event_type, void * p_context)
-{
-	bsp_board_led_invert(0);
-	if(adc_spi_xfer_done) {
-		adc_spi_xfer_done = false;
-		nrf_drv_spi_transfer(&adc_spi, m_tx_buf, m_length, m_rx_buf, m_length);
-	}
-}
 
 static void adc_config_timer(void)
 {
@@ -215,6 +418,116 @@ static void adc_config_timer(void)
 	
 //	nrf_drv_timer_enable(&ADC_SYNC_TIMER);
 }
+
+// BLE advertisement
+static void advertising_init(void)
+{
+    ret_code_t    err_code;
+    ble_advdata_t advdata;
+    ble_advdata_t srdata;
+
+    ble_uuid_t adv_uuids[] = {{SSS_UUID_SERVICE, m_sss.uuid_type}};
+
+    // Build and set advertising data
+    memset(&advdata, 0, sizeof(advdata));
+
+    advdata.name_type          = BLE_ADVDATA_FULL_NAME;
+    advdata.include_appearance = true;
+    advdata.flags              = BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE;
+
+
+    memset(&srdata, 0, sizeof(srdata));
+    srdata.uuids_complete.uuid_cnt = sizeof(adv_uuids) / sizeof(adv_uuids[0]);
+    srdata.uuids_complete.p_uuids  = adv_uuids;
+
+    err_code = ble_advdata_set(&advdata, &srdata);
+    APP_ERROR_CHECK(err_code);
+}
+// BLE connection parameters
+static void conn_params_init(void)
+{
+    ret_code_t             err_code;
+    ble_conn_params_init_t cp_init;
+
+    memset(&cp_init, 0, sizeof(cp_init));
+
+    cp_init.p_conn_params                  = NULL;
+    cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
+    cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
+    cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
+    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
+    cp_init.disconnect_on_fail             = false;
+    cp_init.evt_handler                    = on_conn_params_evt;
+    cp_init.error_handler                  = conn_params_error_handler;
+
+    err_code = ble_conn_params_init(&cp_init);
+    APP_ERROR_CHECK(err_code);
+}
+// BLE GAP parameters
+static void gap_params_init(void)
+{
+    ret_code_t              err_code;
+    ble_gap_conn_params_t   gap_conn_params;
+    ble_gap_conn_sec_mode_t sec_mode;
+
+    BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
+
+    err_code = sd_ble_gap_device_name_set(&sec_mode,
+                                          (const uint8_t *)DEVICE_NAME,
+                                          strlen(DEVICE_NAME));
+    APP_ERROR_CHECK(err_code);
+
+    memset(&gap_conn_params, 0, sizeof(gap_conn_params));
+
+    gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
+    gap_conn_params.max_conn_interval = MAX_CONN_INTERVAL;
+    gap_conn_params.slave_latency     = SLAVE_LATENCY;
+    gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
+
+    err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+    APP_ERROR_CHECK(err_code);
+}
+// BLE GATT
+static void gatt_init(void)
+{
+    ret_code_t err_code = nrf_ble_gatt_init(&m_gatt, NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
+// BLE services
+static void services_init(void)
+{
+    ret_code_t     err_code;
+    ble_sss_init_t init;
+
+    init.led1_write_handler = led_rec_handler;
+	init.led2_write_handler = led_mon_handler;
+    err_code = ble_sss_init(&m_sss, &init);
+    APP_ERROR_CHECK(err_code);
+}
+
+// BLE stack
+static void ble_stack_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_sdh_enable_request();
+    APP_ERROR_CHECK(err_code);
+
+    // Configure the BLE stack using the default settings.
+    // Fetch the start address of the application RAM.
+    uint32_t ram_start = 0;
+    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
+    APP_ERROR_CHECK(err_code);
+
+    // Enable BLE stack.
+    err_code = nrf_sdh_ble_enable(&ram_start);
+    APP_ERROR_CHECK(err_code);
+
+    // Register a handler for BLE events.
+    NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
+}
+
 
 DSTATUS sd_card_test(void)
 {
@@ -431,23 +744,6 @@ FRESULT sd_card_init(void)
 
 
 
-static void sdc_fill_queue(void)
-{
-	bsp_board_led_invert(2);
-	static FRESULT res;
-	static UINT byte_written;
-	static uint8_t p_buf[2*SDC_BLOCK_SIZE];
-	uint32_t buf_size = 2*SDC_BLOCK_SIZE;
-	uint32_t fifo_res = app_fifo_read(&m_adc2sd_fifo, p_buf, &buf_size);
-	static app_fifo_t * p_fifo = &m_adc2sd_fifo;
-//	NRF_LOG_INFO("read: %d", p_fifo->read_pos);
-	sdc_writing = true;
-	res = f_write(&recording_fil, p_buf, (2*SDC_BLOCK_SIZE), &byte_written);
-	if(res == FR_OK) {
-		res = f_sync(&recording_fil);
-		sdc_writing = false;
-	}
-}
 
 
 /**
@@ -457,6 +753,10 @@ int main(void)
 {
 	DSTATUS card_status;
 	FRESULT ff_result;
+	ret_code_t err_code;
+	
+	err_code = app_timer_init();
+	APP_ERROR_CHECK(err_code);
 	
     bsp_board_leds_init();
 	
@@ -467,9 +767,15 @@ int main(void)
 	NRF_LOG_INFO("FATFS + SD + SPI example.");
     NRF_LOG_INFO("-------------------------")
 
+	ble_stack_init();
+	gap_params_init();
+	gatt_init();
+	services_init();
+	advertising_init();
+	conn_params_init();
+
 	adc_config_spi();
 	adc_config_timer();
-	
 	app_fifo_init(&m_adc2sd_fifo, m_fifo_buffer, FIFO_DATA_SIZE);
 
 	card_status = sd_card_test();
